d5908ff83f3dad429be192a33d367a78ed4d4208
jesustgr <jesustgreyes@gmail.com>
2025-03-11 05:46:37 -0700

    install dependencies and configure typeorm

--- task-management-api/src/app.module.ts
+++ task-management-api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // import typeorm
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', 
+      port: 5432,       
+      username: 'postgres', // my username
+      password: 'ballislife', // my password
+      database: 'task_management', 
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Entities path
+      synchronize: true, // ONLY FOR DEVELOPMENT
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
